a4bd6620668d4e52335223d4e45dc51f33ebcf6a
nyoewono <nathanael9721@gmail.com>
2020-05-07 15:57:41 +1000

    template progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return ([]);
+import Nav from "./components/nav";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        {/* the content */}
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+
---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Roommee</h1>
+    </div>
+  );
+}
