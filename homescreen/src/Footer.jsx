63979f2e7a44afcaff02eacd7e7c33c96e491a5f
rahimz98 <rahimz@student.unimelb.edu.au>
2020-05-14 23:15:11 +1000

    Adding files for home page

---
+++ homescreen/src/Footer.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import {Typography, Grid, makeStyles, Link } from '@material-ui/core';
+import roommeeLogo from './roommeeLogo.png';
+import lineart from './lineart.png'
+
+const useStyles = makeStyles((theme) => ({
+    topPic: {
+        height: "150px",
+        backgroundImage: `url(${lineart})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "repeat-x",
+    },
+    headingStyle: {
+        backgroundColor: "#FCDB87",
+        paddingLeft: "27px"
+    },
+    linkGridStyle: {
+        backgroundColor: "#f9eadf",
+        height: "50px",
+        paddingRight: "27px"
+    },
+    linkTextStyle: {
+        paddingRight: "27px"
+    }
+}));
+
+export default function Footer() {
+    const classes = useStyles();
+
+    return (
+        <Grid
+            container
+            direction="column"
+            justify="flex-end"
+            alignItems="stretch"
+        >
+            <Grid className={classes.topPic}/>
+            <Grid className={classes.headingStyle}>
+                <Grid
+                    container
+                    direction="column"
+                    justify="flex-end"
+                    alignItems="flex-start"
+                >
+                    <Grid>
+                        <Link href='/'>
+                            <img src={roommeeLogo} alt="" height="75px" />
+                        </Link>
+                    </Grid>
+                    <Grid>
+                        <Typography variant='h5'>
+                            By Paradigm
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid
+                container
+                direction="row"
+                justify="flex-end"
+                alignItems="flex-end"
+                className={classes.linkGridStyle}
+            >
+                <Grid className={classes.linkTextStyle}>
+                    <Typography variant='body1'>
+                        <Link href="/">Terms</Link>
+                    </Typography>
+                </Grid>
+                <Grid className={classes.linkTextStyle}>
+                    <Typography variant='body1'>
+                        <Link href="/">Settings</Link>
+                    </Typography>
+                </Grid>
+                <Grid className={classes.linkTextStyle}>
+                    <Typography variant='body1'>
+                        <Link href="/">Help</Link>
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+};
---
+++ homescreen/src/content.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Button} from "@material-ui/core";
+import { Grid } from '@material-ui/core';
+import {makeStyles} from "@material-ui/styles";
+import Logo from './roommeeLogo.png';
+
+
+const useStyles = makeStyles(() => ({
+
+    buttonStyles: {
+        background: '#FCDB87',
+        color: 'white',
+
+        fontWeight: 'bold',
+        fontSize: 'large',
+        maxWidth: '600px',
+        maxHeight: '75px',
+        minWidth: '100px',
+        minHeight: '75px'
+    }
+
+}));
+
+const Content = () => {
+    const classes = useStyles();
+
+    return (
+        <Grid container direction='column' alignItems="center" justify="space-evenly" style={{ minHeight: '70vh' }}>
+
+            <Grid item>
+                <img src={Logo} />
+            </Grid>
+
+            <Grid item container spacing={0} alignItems="center" justify="space-evenly">
+                <Grid item >
+                    <Button
+                        variant='contained'
+                        className= {classes.buttonStyles}
+                        href = '/'
+                    >
+                        List Property
+                    </Button>
+                </Grid>
+                <Grid item >
+                    <Button
+                        variant='contained'
+                        className={classes.buttonStyles}
+                        href = '/'
+                    >
+                        Find Roommee
+                    </Button>
+                </Grid>
+            </Grid>
+            <Grid item>
+                <Button
+                    variant='contained'
+                    className={classes.buttonStyles}
+                    href = '/'
+                >
+                    View Matches
+                </Button>
+            </Grid>
+        </Grid>
+
+)};
+
+export default Content
